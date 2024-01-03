216473de20b6fd35303a40f2c6260196ee70b1e2
sarifmanung <406168026@yru.ac.th>
2024-01-03 19:32:46 +0700

    replace moment with dayjs library

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const moment = require("moment");
+const dayjs = require("dayjs");
 
 function calculateWorkExperience(timeframes) {
   let totalYears = 0;
@@ -26,18 +26,22 @@ function mergeOverlappingTimeframes(timeframes) {
 
   // Sort the timeframes based on the start date
   const sortedTimeframes = timeframes.sort((a, b) =>
-    moment(a[0], "DD/MM/YYYY").diff(moment(b[0], "DD/MM/YYYY"))
+    dayjs(a[0], "DD/MM/YYYY").diff(dayjs(b[0], "DD/MM/YYYY"))
   );
 
   let [currentStart, currentEnd] = sortedTimeframes[0];
 
   for (let i = 1; i < sortedTimeframes.length; i++) {
     const [start, end] = sortedTimeframes[i];
-    if (moment(start, "DD/MM/YYYY").isSameOrBefore(currentEnd)) {
+    if (
+      dayjs(start, "DD/MM/YYYY").isSame(currentEnd) ||
+      dayjs(start, "DD/MM/YYYY").isBefore(currentEnd)
+    ) {
       // Merge overlapping timeframes
-      currentEnd = moment
-        .max(moment(currentEnd, "DD/MM/YYYY"), moment(end, "DD/MM/YYYY"))
-        .format("DD/MM/YYYY");
+      currentEnd = dayjs.max(
+        dayjs(currentEnd, "DD/MM/YYYY"),
+        dayjs(end, "DD/MM/YYYY")
+      );
     } else {
       // Add the merged timeframe and update currentStart, currentEnd
       merged.push([currentStart, currentEnd]);
@@ -55,11 +59,11 @@ function calculateDuration(timeframe) {
   const startDate = parseDate(timeframe[0]);
   const endDate = parseDate(timeframe[1]);
 
-  const duration = moment.duration(endDate.diff(startDate));
+  const diffInDays = dayjs(endDate).diff(startDate, "day");
 
-  const years = Math.floor(duration.asDays() / 365);
-  const months = Math.floor((duration.asDays() % 365) / 30);
-  const days = Math.floor(duration.asDays() % 30);
+  const years = Math.floor(diffInDays / 365);
+  const months = Math.floor((diffInDays % 365) / 30);
+  const days = Math.floor(diffInDays % 30);
 
   return { years, months, days };
 }
@@ -67,10 +71,17 @@ function calculateDuration(timeframe) {
 function parseDate(dateStr) {
   // Assuming the date format is DD/MM/YYYY
   const [day, month, year] = dateStr.split("/").map(Number);
-  return moment(`${year}-${month}-${day}`, "YYYY-MM-DD");
+  return dayjs(`${year}-${month}-${day}`, "YYYY-MM-DD");
 }
 
-// Example usage:
+// const timeframes = [
+//   ["01/01/2558", "31/12/2560"],
+//   ["01/01/2555", "31/12/2555"],
+//   ["01/12/2559", "31/12/2559"],
+//   ["01/06/2556", "31/12/2556"],
+//   ["01/06/2557", "31/05/2558"],
+// ];
+
 const timeframes = [
   ["01/01/2558", "31/12/2560"],
   ["01/01/2555", "31/12/2555"],
